cc7fed61acb2b269bdc9b3276566ab356836a078
Abdur Rauf <abdurrauf687@gmail.com>
2026-06-20 16:14:51 +0400

    questions.py code added

---
+++ questions.py
@@ -0,0 +1,27 @@
+QUESTIONS = [
+    {
+        "question": "What is the capital of Pakistan? 🇵🇰",
+        "options": ["Lahore", "Karachi", "Islamabad", "Peshawar"],
+        "answer": "Islamabad"
+    },
+    {
+        "question": "Which planet is closest to the Sun? ☀️",
+        "options": ["Venus", "Mercury", "Earth", "Mars"],
+        "answer": "Mercury"
+    },
+    {
+        "question": "How many sides does a hexagon have?",
+        "options": ["5", "6", "7", "8"],
+        "answer": "6"
+    },
+    {
+        "question": "What is 12 x 12?",
+        "options": ["124", "144", "132", "112"],
+        "answer": "144"
+    },
+    {
+        "question": "Who invented the telephone? 📞",
+        "options": ["Edison", "Tesla", "Bell", "Newton"],
+        "answer": "Bell"
+    },
+]
